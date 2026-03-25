969caff9c1b3b8995f7872b31b9af8a947ba6d9c
Roman <dodin.roman@gmail.com>
2026-03-25 15:47:39 +0100

    adding config intent

--- bottom_toolbar/intents/bottomtoolbar/config_intent.py
+++ bottom_toolbar/intents/bottomtoolbar/config_intent.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
 import eda_common as eda
-import utils.node_utils as nutils
-import utils.exceptions as e
+
 import bottom_toolbar.api.v1alpha1.pysrc.constants as c
-
-from common.constants import PLATFORM_SRL, PLATFORM_SROS
-from utils.log import log_msg
-
+import utils.exceptions as e
+import utils.node_utils as nutils
 from bottom_toolbar.api.v1alpha1.pysrc.bottomtoolbar import BottomToolbar
 from bottom_toolbar.intents.bottomtoolbar.handlers import get_config_handler
 from bottom_toolbar.intents.bottomtoolbar.init import init_globals_defaults, validate
+from common.constants import PLATFORM_SRL, PLATFORM_SROS
+from common.metadata import Y_METADATA, Y_NAME
+from utils.log import log_msg
+
 
 def process_cr(cr):
     """Process BottomToolbar CR."""
@@ -18,6 +19,16 @@ def process_cr(cr):
     if cr_obj is None:
         return
 
-    cr_name = cr_obj.metadata.name
     validate(cr_obj)
     init_globals_defaults(cr_obj)
+
+    for node in nutils.list_nodes(filter=[], label_filter=[], ns=cr_obj.metadata.namespace):
+        log_msg(f"Node: {node[Y_METADATA][Y_NAME]}")
+
+        if node.get("spec", None) is not None and node["spec"].get("operatingSystem", None) is not None:
+            if node["spec"]["operatingSystem"] == PLATFORM_SRL:
+                srl_handler = get_config_handler(PLATFORM_SRL)
+                srl_handler.handle_cr(cr_obj, node_cr=node)
+            else:
+                log_msg(f"Node OS not supported: {node['spec']['operatingSystem']}")
+                continue
--- bottom_toolbar/intents/bottomtoolbar/srl.py
+++ bottom_toolbar/intents/bottomtoolbar/srl.py
@@ -2,12 +2,50 @@
 import json
 
 import eda_common as eda
+
 import utils.schema as s
-from common.metadata import Y_METADATA, Y_NAME
 from bottom_toolbar.api.v1alpha1.pysrc.bottomtoolbar import BottomToolbar
+from common.metadata import Y_METADATA, Y_NAME
+from core.pysrc import nodeconfig
+from core.pysrc.nodeconfig import NodeConfig, NodeConfigTupleSpec
+from utils.log import log_msg
 
 
 class SrlBaseConfigHandler:
-    def handle_cr(self, cr_obj: BottomToolbar):
-        # implement this
-        pass
+    def handle_cr(self, cr_obj: BottomToolbar, node_cr=None):
+        log_msg(f"cr_obj: {cr_obj}")
+        log_msg(f"node_cr: {node_cr}")
+
+        node_name = node_cr[Y_METADATA][Y_NAME]
+
+        configs = self._generate_config(cr_obj)
+
+        node_config = NodeConfig(
+            metadata=nodeconfig.Metadata(
+                name=f"bottom-toolbar-{cr_obj.metadata.name}-{node_name}",
+            ),
+            spec=nodeconfig.NodeConfigSpec(
+                node_endpoint=node_name,
+                configs=configs,
+            ),
+        )
+
+        eda.update_cr(**node_config.to_input())
+
+    def _generate_config(self, cr_obj: BottomToolbar) -> list[NodeConfigTupleSpec]:
+        configs = []
+        # cli config (in .system.cli context)
+        _config = {}
+
+        if cr_obj.spec.message:
+            _config.setdefault("cli", {}).setdefault("environment", {})["bottom-toolbar"] = cr_obj.spec.message
+
+        configs.append(
+            {
+                "path": ".system",
+                "config": json.dumps(_config),
+                "operation": "Create",
+            },
+        )
+
+        return configs
